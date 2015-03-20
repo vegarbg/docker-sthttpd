2fc2b45d0718f4490c8651601cbdbe26e785e193
Vegar Guldal <vegar@guldal.no>
2015-03-20 12:32:45 +0100

    Update sthttpd version

--- buildpackage.sh
+++ buildpackage.sh
@@ -3,7 +3,7 @@
 rm -rf /tmp/build /tmp/dist /tmp/*.gz
 apt-get update
 apt-get install -y build-essential automake curl
-curl ftp://opensource.dyc.edu/pub/sthttpd/sthttpd-2.26.3.tar.gz > /tmp/sthttpd.tar.gz
+curl ftp://opensource.dyc.edu/pub/sthttpd/sthttpd-2.27.0.tar.gz > /tmp/sthttpd.tar.gz
 mkdir -p /tmp/build /tmp/dist
 cd /tmp/build
 tar zxvf ../sthttpd.tar.gz
